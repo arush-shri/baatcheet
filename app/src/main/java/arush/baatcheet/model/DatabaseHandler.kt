83efee293af8225d55e1489442ff65c5a47c0619
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-10-05 23:25:13 +0530

    send msg logic

--- app/src/main/java/arush/baatcheet/model/DatabaseHandler.kt
+++ app/src/main/java/arush/baatcheet/model/DatabaseHandler.kt
@@ -1,9 +1,15 @@
 package arush.baatcheet.model
 
 import android.net.Uri
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
+import org.json.JSONArray
+import org.json.JSONObject
 
 class DatabaseHandler {
     private val database : DatabaseReference = FirebaseDatabase.getInstance().getReference("baatcheet")
@@ -11,7 +17,7 @@ class DatabaseHandler {
     fun login(username: String, phoneNumber: String, imageUri: Uri?)
     {
         if (imageUri != null) {
-            val imageRef = storage.getReference("DP").child(phoneNumber + System.currentTimeMillis())
+            val imageRef = storage.getReference("DP").child(phoneNumber + "DP")
             imageRef.putFile(imageUri)
                 .addOnSuccessListener {
                     imageRef.downloadUrl.addOnSuccessListener { uri ->
@@ -26,7 +32,27 @@ class DatabaseHandler {
                 }
         }
     }
-    fun sendMessage(message: String, receiver: String){
+    fun sendMessage(msg: String, sender: String, receiver: String){
+        val timeStamp = ""
+        var message: JSONArray? = null
+        val msgObject = JSONObject()
+        database.child(receiver).child("messageList").child(sender).child("messages")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    message = dataSnapshot.getValue(JSONArray::class.java)
+                } else {
+                    message = JSONArray()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.d("DBError", databaseError.message)
+            }
+        })
 
+        msgObject.put(timeStamp, msg)
+        message!!.put(msgObject)
+
+        database.child(receiver).child("messageList").child(sender).child("messages").setValue(message)
     }
 }
